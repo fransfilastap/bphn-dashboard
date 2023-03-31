411a3c48734b15ef63dec0c20ee0762804c135a6
fransfilastap@live.com <fransfilastap@Live.com>
2023-03-31 12:58:11 +0700

    fix: key filter

--- src/app/(program)/legislasi/program-select.tsx
+++ src/app/(program)/legislasi/program-select.tsx
@@ -34,7 +34,7 @@ export default function ProgramSelect ({programs}:{programs:Program[]}) {
               className="bg-gray-50 border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option defaultValue={""}>Filter Program</option>
         {programs.map((e: Program, i: number) => (
-          <option value={e.id}>{e.name}</option>
+          <option key={e.id} value={e.id}>{e.name}</option>
         ))}
       </select>
     </div>
